bb41020371a79d645ac715786ba1cf6f21f94752
zhangheng <zhangheng@qq.com>
2022-04-24 12:11:01 +0800

    zhang master commit three

--- hello.java
+++ hello.java
@@ -2,7 +2,7 @@ pulic class demo{
   public static void main(String[] args){
   System.out.println("hello");
 zhangheng  bi  ni suai xiang xiao a!!!!!!!!
-  System.out.println("nihao");
+  System.out.println("nihao");6666
   }
 }
 
